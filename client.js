3011e4c3ea5cfc26629ef8accbf86e4941267bc2
Joan Tammo <gwantamo35@gmail.com>
2023-11-23 02:03:49 +0100

    reformatted

--- client.js
+++ client.js
@@ -3,12 +3,11 @@ const net = require('net');
 const client = new net.Socket();
 
 const PORT = 3001;
-const SERVER_IP = '127.0.0.1'; // Change this to the IP address of your server if it's not running locally
+const SERVER_IP = '127.0.0.1'; 
 
 client.connect(PORT, SERVER_IP, () => {
   console.log('Connected to TCP server');
 
-  // Handle data received from the server
   client.on('data', (data) => {
     try {
       const receivedData = JSON.parse(data.toString());
@@ -18,18 +17,16 @@ client.connect(PORT, SERVER_IP, () => {
     }
   });
 
-  // Handle the connection being closed
+  
   client.on('close', () => {
     console.log('Connection to server closed');
   });
 });
 
-// Handle errors during connection
 client.on('error', (err) => {
   console.error(`Error connecting to server: ${err.message}`);
 });
 
-// Handle the client's termination
 process.on('SIGINT', () => {
   client.destroy();
   console.log('Client terminated');
--- motionSensorServer.js
+++ motionSensorServer.js
@@ -1,19 +1,24 @@
-const express = require('express');
-const net = require('net');
-const mongoose = require('mongoose');
-const axios = require('axios');
+const express = require("express");
+const net = require("net");
+const mongoose = require("mongoose");
+const axios = require("axios");
 
 const app = express();
 
-mongoose.connect("mongodb+srv://ameyamahankal:4mq7FaDOD8LzQg18@cluster0.sjpz1jh.mongodb.net/", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://ameyamahankal:4mq7FaDOD8LzQg18@cluster0.sjpz1jh.mongodb.net/",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const sendMotionDataToClient = async (socket) => {
   try {
     while (true) {
-      const response = await axios.get('http://localhost:3000/motion/getMotion');
+      const response = await axios.get(
+        "http://localhost:3000/motion/getMotion"
+      );
       const detectionValue = response.data.detection;
 
       console.log(`Fetched motion data from Express server: ${detectionValue}`);
@@ -21,21 +26,24 @@ const sendMotionDataToClient = async (socket) => {
       const jsonData = JSON.stringify({ detection: detectionValue });
       socket.write(jsonData);
 
-      await new Promise(resolve => setTimeout(resolve, 1000));
+      await new Promise((resolve) => setTimeout(resolve, 1000));
     }
   } catch (error) {
-    console.error('Error getting motion data from Express server:', error.message);
+    console.error(
+      "Error getting motion data from Express server:",
+      error.message
+    );
   }
 };
 
 const server = net.createServer((socket) => {
-  console.log('Client connected');
+  console.log("Client connected");
 
   // Send motion data to the client continuously when it connects
   sendMotionDataToClient(socket);
 
-  socket.on('end', () => {
-    console.log('Client disconnected');
+  socket.on("end", () => {
+    console.log("Client disconnected");
   });
 });
 
@@ -44,8 +52,6 @@ server.listen(PORT, () => {
   console.log(`TCP Server running on port ${PORT}`);
 });
 
-app.get('/status', (req, res) => {
-  res.json({ status: 'Server is running' });
+app.get("/status", (req, res) => {
+  res.json({ status: "Server is running" });
 });
-
-
