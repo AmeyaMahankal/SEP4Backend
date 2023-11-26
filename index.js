b3dff422c1fc08a2d68e7095e3ff34c13dd4e937
Joan Tammo <gwantamo35@gmail.com>
2023-11-26 18:20:52 +0100

    created a new artifacts model
    created a new artifacts model, and created a new endpoints for them

--- index.js
+++ index.js
@@ -19,16 +19,16 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-const routes = require('./routes/routes');
 const temps = require('./routes/Temperatures');
 const humid = require('./routes/Humidities');
 const lights = require('./routes/LightLevels');
 const motion = require('./routes/Motions');
-app.use('/api', routes)
+const artifacts = require('./routes/Artifacts');
 app.use('/temp', temps)
 app.use('/light', lights)
 app.use('/humid', humid)
 app.use('/motion', motion)
+app.use('/artifacts', artifacts)
 
 app.get("/", (req, res) => {
     res.send("<h2>Express Server running on port 3000</h2>")
