13e51af3e09efc13069c696dfa5cd5f20af31f0f
Joan Tammo <gwantamo35@gmail.com>
2023-12-02 00:23:21 +0100

    added a client to the post endpoint for the pin code

--- routes/PinCodes.js
+++ routes/PinCodes.js
@@ -1,55 +1,68 @@
 const express = require('express');
-const PinCodeModel = require('../model/PinCodeModel');
-
 const router = express.Router();
+const net = require('net'); 
 
-// GET method to retrieve matching pin code
-router.get('/getPin', async (req, res) => {
-  try {
-    const enteredPin = req.body.pinCode;
+const PinCodeModel = require('../model/PinCodeModel'); 
 
-    if (!enteredPin) {
-      return res.status(400).json({ error: 'Please provide a pin code.' });
-    }
-
-    const matchingPin = await PinCodeModel.findOne({ pinCode: enteredPin });
-    if (!matchingPin) {
-      return res.status(404).json({ error: 'No matching pin code found.' });
-    }
-
-    res.json(matchingPin);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// POST method to compare entered pin code
 router.post('/comparePin', async (req, res) => {
-  try {
-    const enteredPin = req.body.pinCode;
+    const handleTcpClient = () => {
+        return new Promise((resolve, reject) => {
+            const client = new net.Socket();
+            const serverAddress = '127.0.0.1';
+            const serverPort = 23;
 
-    if (!enteredPin) {
-      return res.status(400).json({ error: 'Please provide a pin code.' });
+            client.connect(serverPort, serverAddress, () => {
+                console.log('Connected to TCP server');
+
+                client.write('ChangeSecurityStatus');
+            });
+
+            client.on('error', (error) => {
+                console.error('Error in TCP socket connection:', error);
+                reject(error);
+
+                client.end();
+            });
+
+            client.on('close', () => {
+                console.log('Connection to TCP server closed');
+            });
+
+            client.on('data', (data) => {
+                console.log('Received data from TCP server:', data.toString());
+
+                client.end();
+
+                resolve(data.toString());
+            });
+        });
+    };
+
+    try {
+        const enteredPin = req.body.pinCode;
+
+        if (!enteredPin) {
+            return res.status(400).json({ error: 'Please provide a pin code.' });
+        }
+
+        const storedPin = await PinCodeModel.findOne({});
+
+        if (!storedPin) {
+            return res.status(404).json({ error: 'No pin code found in the database.' });
+        }
+
+        const isMatch = enteredPin == storedPin.pinCode; 
+
+        if (isMatch) {
+            const tcpResponse = await handleTcpClient();
+
+            res.json({ message: 'Unlocked!', tcpResponse });
+        } else {
+            res.status(401).json({ error: 'Pin codes do not match.' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
-
-    // Retrieve the stored pin code from the database
-    const storedPin = await PinCodeModel.findOne({});
-
-    if (!storedPin) {
-      return res.status(404).json({ error: 'No pin code found in the database.' });
-    }
-
-    // Compare the entered pin code with the stored pin code
-    const isMatch = enteredPin === storedPin.pinCode;
-
-    if (isMatch) {
-      res.json({ message: 'Pin codes match!' });
-    } else {
-      res.status(401).json({ error: 'Pin codes do not match.' });
-    }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
 });
 
 module.exports = router;
