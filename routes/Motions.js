6edb69f73dee9808370d21f718e0730ac899a8f3
KingAntoni <agoreta1234@gmail.com>
2023-12-05 13:38:25 +0100

    tests and seperate endpoints

--- routes/Motions.js
+++ routes/Motions.js
@@ -1,111 +1,36 @@
 const express = require('express');
-const MotionModel = require('../model/MotionModel');
-const net = require('net');
+const motionService = require('../services/MotionService');
 
 const router = express.Router();
 
+router.post('/post', async (req, res) => {
+  const { detection } = req.body;
 
-router.post('/motion', async (req, res) => {
-    try {
+  try {
+    const dataToSave = await motionService.saveMotion(detection);
+    res.status(200).json(dataToSave);
 
-        const { detection } = req.body;
-
-        const newMotionData = new MotionModel({ detection });
-
-
-        await newMotionData.save();
-
-        res.status(200).json({ message: 'Motion data saved' });
-    } catch (error) {
-        console.error('Error saving motion data:', error);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-//GET
-
-router.get('/getMotion', async (req, res) => {
-    try {
-        const motion = await MotionModel.findOne().sort({ time: -1 });
-
-        if (motion) {
-            res.json({ detection: motion.detection });
-        } else {
-            res.status(404).json({ message: 'no data' });
-        }
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+router.get('/getMotions', async (req, res) => {
+  try {
+    const data = await motionService.getAllMotions();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
-module.exports = router;
-
-// PATCH
 
 router.patch('/updateMotion', async (req, res) => {
-    try {
-        // Connect to the TCP server
-        const client = new net.Socket();
-        const serverAddress = '192.168.214.98';
-        const serverPort = 23;
-
-        client.connect(serverPort, serverAddress, () => {
-            console.log('Connected to TCP server');
-
-            // Send a message to the TCP server
-            client.write('ChangeSecurityStatus');
-        });
-
-        // Set up event handlers for data received from the TCP server
-        client.on('data', async (data) => {
-            // Print the received data to the console
-            console.log('Received data from TCP server:', data.toString());
-
-            if (data == "SSCRemote") {
-                try {
-
-
-                    const latestMotionData = await MotionModel.findOne().sort({ time: -1 });
-
-                    if (latestMotionData) {
-                        latestMotionData.detection = !latestMotionData.detection;
-                        await latestMotionData.save();
-
-                        res.json({ message: 'Motion status updated successfully' });
-                    } else {
-                        res.status(404).json({ message: 'No data to update' });
-                    }
-                } catch (error) {
-                    res.status(500).json({ message: error.message });
-                }
-
-                client.end();
-
-            }
-
-            // Close the connection after receiving the data
-        });
-
-        // Handle socket connection errors
-        client.on('error', (error) => {
-            console.error('Error in TCP socket connection:', error);
-            res.status(500).json({ error: 'Internal Server Error' });
-
-            // Close the connection in case of an error
-            client.end();
-        });
-
-        // Handle the socket connection closure
-        client.on('close', () => {
-            console.log('Connection to TCP server closed');
-        });
-    } catch (error) {
-        // Handle other errors in your try-catch block if needed
-        console.error('Error in /getTemperatures endpoint:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-
+  try {
+    const updatedData = await motionService.updateMotionStatus();
+    res.json(updatedData);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = router;
-
-
---
+++ services/MotionService.js
@@ -0,0 +1,67 @@
+const motionModel = require('../model/MotionModel');
+const net = require('net');
+
+class MotionService {
+  async saveMotion(detection) {
+    const data = new motionModel({ detection });
+
+    try {
+      const dataToSave = await data.save();
+      return dataToSave;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getAllMotions() {
+    try {
+      const data = await motionModel.find().sort({ time: -1 });
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updateMotionStatus() {
+    const client = new net.Socket();
+    const serverAddress = '192.168.214.98';
+    const serverPort = 23;
+
+    return new Promise((resolve, reject) => {
+      client.connect(serverPort, serverAddress, () => {
+        client.write('ChangeSecurityStatus');
+      });
+
+      client.on('data', async (data) => {
+        if (data.toString() === 'SSCRemote') {
+          try {
+            const latestMotionData = await motionModel.findOne().sort({ time: -1 });
+
+            if (latestMotionData) {
+              latestMotionData.detection = !latestMotionData.detection;
+              await latestMotionData.save();
+              resolve({ message: 'Motion status updated successfully' });
+            } else {
+              reject({ status: 404, message: 'No data to update' });
+            }
+          } catch (error) {
+            reject({ status: 500, message: error.message });
+          }
+
+          client.end();
+        }
+      });
+
+      client.on('error', (error) => {
+        reject({ status: 500, message: 'Internal Server Error', error });
+        client.end();
+      });
+
+      client.on('close', () => {
+        console.log('Connection to TCP server closed');
+      });
+    });
+  }
+}
+
+module.exports = new MotionService();
